b51a497de225dd5a4ed954400c0f5ab77c5e70db
Frizz123 <frizz991@gmail.com>
2016-10-17 14:36:21 +0300

    Добавлены исправления для CP

--- contenteditable-autocomplete.js
+++ contenteditable-autocomplete.js
@@ -130,10 +130,21 @@
                     if (isMultiple) addTrailingComma()
                 }
 
-                //
-                function handleInput(/* event */) {
-                    var newValue = $input.text()
-                    var query
+        function handleClick() {
+            currentValue = $input.text()
+
+            if (currentValue.length == 0) {
+                event.stopPropagation()
+                $('.suggestions').hide()
+                $input.trigger('autocomplete:request', [currentValue, handleNewSuggestions])
+            }
+
+        }
+
+        //
+        function handleInput(/* event */) {
+            var newValue = $input.text()
+            var query
 
                     if (!newValue.trim()) {
                         $suggestions.hide()
@@ -192,21 +203,21 @@
                     }
                 }
 
-                //
-                function handleBlur(/* event */) {
-                    $suggestions.hide()
+        function handleBlur(event) {
+            /*$suggestions.hide()
 
-                    if (isMultiple) removeTrailingComma()
-                }
+             if (isMultiple) removeTrailingComma()*/
+        }
 
-                //
-                function handleSuggestionClick(event) {
-                    event.preventDefault()
-                    event.stopPropagation()
+        function handleSuggestionClick(event) {
+            if (event.target.className != 'external-directive-documentation-link') {
+                event.preventDefault()
+                event.stopPropagation()
 
-                    selectSuggestionByElement($(event.currentTarget))
-                    $suggestions.hide()
-                }
+                selectSuggestionByElement($(event.currentTarget))
+                $suggestions.hide()
+            }
+        }
 
                 // Internal Methods
                 // ----------------
@@ -230,21 +241,29 @@
                         return
                     }
 
-                    currentSuggestions.forEach(function (suggestion, index) {
-                        var label = suggestion.label
-                        var highlight = (index === 0) ? ' class="highlight"' : ''
+            currentSuggestions.forEach(function (suggestion, index) {
+                var help = suggestion.value.split(';')[1]
+                var link = suggestion.value.split(';')[2]
+                suggestion.label = suggestion.value = suggestion.value.split(';')[0]
+                suggestion.value = '<span class="directive-name">' + suggestion.value + '</span> =&nbsp;'
+                var label = suggestion.label
+                var highlight = (index === 0) ? ' class="highlight"' : ''
 
                         if (!label) return
 
                         label = htmlEscape(label)
 
-                        // select first result per default
-                        html += '<div' + highlight + '>'
-                        html += label.replace(regex, '<strong>$1</strong>')
-                        html += '</div>'
-                    })
-                    $suggestions.html(html).show()
-                }
+                // select first result per default
+                html += '<div' + highlight + '><span class="select-directive-item">'
+                html += label.replace(regex, '<strong>$1</strong>')
+                html += '<div class="directive-help">'
+                if (link && link != null)
+                    html += '<a href="http://php.net/manual/' + t('ru') + '/' + link + '" target="_blank" class="external-directive-documentation-link"><i class="fa fa-external-link" aria-hidden="true"></i>&nbsp;' + t('Описание директивы') + '</a> '
+                html += t('Тип значения') + ': ' + help + '</div></span></div>'
+            })
+            $suggestions.html(html).show()
+            $suggestions.scrollTop(0)
+        }
 
                 //
                 function newSuggestionsOnly(suggestion) {
@@ -272,9 +291,10 @@
 
                     if (!$next.length) return
 
-                    $highlighted.removeClass('highlight')
-                    $next.addClass('highlight')
-                }
+            $highlighted.removeClass('highlight')
+            $next.addClass('highlight')
+            $suggestions.scrollTop($suggestions.scrollTop() + 45)
+        }
 
                 //
                 function highlightPreviousSuggestion() {
@@ -283,9 +303,11 @@
 
                     if (!$prev.length) return
 
-                    $highlighted.removeClass('highlight')
-                    $prev.addClass('highlight')
-                }
+            $highlighted.removeClass('highlight')
+            $prev.addClass('highlight')
+
+            $suggestions.scrollTop($suggestions.scrollTop() - 45)
+        }
 
                 //
                 function selectHighlightedSuggestion() {
@@ -307,16 +329,17 @@
                     $input.trigger('autocomplete:select', [selected])
                 }
 
-                function setCursorAt(position) {
-                    var range = document.createRange()
-                    var sel = window.getSelection()
-                    var textNode = $input[0].childNodes.length ? $input[0].childNodes[0] : $input[0]
-                    position = Math.min(textNode.length, position)
-                    range.setStart(textNode, position)
-                    range.collapse(true)
-                    sel.removeAllRanges()
-                    sel.addRange(range)
-                }
+        function setCursorAt() {
+            if ($input[0].childNodes[1]) {
+                var range = document.createRange()
+                var sel = window.getSelection()
+                var textNode = $input[0].childNodes[1]
+                range.setStart(textNode, textNode.length)
+                range.collapse(true)
+                sel.removeAllRanges()
+                sel.addRange(range)
+            }
+        }
 
                 //
                 // to find out what the current word is, we get the current
